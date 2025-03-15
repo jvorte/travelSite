d789587c984e945767198540028f948fa6e9ad7c
Dimitris <jvortelinas@gmail.com>
2025-03-15 14:51:07 +0100

    footer ok

--- resources/views/trips-tips.blade.php
+++ resources/views/trips-tips.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <h2 class="display-2 text-center ">Trips & Tips</h2>
-        <p  class="display-5 text-center " >Here you can find travel tips and recommendations for your next adventure!</p>
+        <p  class="display-6 text-center " >Here you can find travel tips and recommendations for your next adventure!</p>
 
         <!-- Εδώ μπορείς να προσθέσεις περιεχόμενο για ταξίδια και συμβουλές -->
     </div>
