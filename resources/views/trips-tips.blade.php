dbd34bd57cbbd3ac9b84ead687cd7b48e1ed7f78
Dimitris <jvortelinas@gmail.com>
2025-03-16 09:46:38 +0100

    branch start

--- resources/views/trips-tips.blade.php
+++ resources/views/trips-tips.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container title">
         <h1 class="display-4 text-center ">Trips & Tips</h1>
-        <h5  class=" text-center " >Here you can find travel tips and recommendations for your next adventure!</h5>
+        <p  class=" text-center " >Here you can find travel tips and recommendations for your next adventure!</p>
 
         <!-- Εδώ μπορείς να προσθέσεις περιεχόμενο για ταξίδια και συμβουλές -->
     </div>
